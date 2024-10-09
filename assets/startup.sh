677fbc27cf0e514e0441571cb51e64c9c3be8113
Mingye Wang <arthur200126@gmail.com>
2024-10-09 13:54:18 +0000

    startup.sh: use /bin/bash
    We already use some bashisms in it. The fact that it's working indicates that, well, we're using bash. So let's use some more to make our lives easier.

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 DEFAULT_NTP="0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org,3.pool.ntp.org"
 CHRONY_CONF_FILE="/etc/chrony/chrony.conf"
@@ -38,29 +38,29 @@ if [ -z "${LOG_LEVEL}" ]; then
   LOG_LEVEL=0
 else
   # confirm log level is between 0-3, since these are the only log levels supported
-  if [ "${LOG_LEVEL}" -gt 3 ]; then
+  if [[ $LOG_LEVEL != [0123] ]]; then
     # level outside of supported range, let's set to default (0)
     LOG_LEVEL=0
   fi
 fi
 
-IFS=","
-for N in $NTP_SERVERS; do
+IFS="," read -ra ntp_servers <<< "$NTP_SERVERS"
+for N in "${ntp_servers[@]}"; do
   # strip any quotes found before or after ntp server
   N_CLEANED=${N//\"}
 
   # check if ntp server has a 127.0.0.0/8 address (RFC3330) indicating it's
   # the local system clock
   if [[ "${N_CLEANED}" == "127\."* ]]; then
-    echo "server "${N_CLEANED} >> ${CHRONY_CONF_FILE}
+    echo "server ${N_CLEANED}" >> ${CHRONY_CONF_FILE}
     echo "local stratum 10"    >> ${CHRONY_CONF_FILE}
 
   # found external time servers
   else
     if [[ "${ENABLE_NTS:-false}" = true ]]; then
-      echo "server "${N_CLEANED}" iburst nts" >> ${CHRONY_CONF_FILE}
+      echo "server ${N_CLEANED} iburst nts" >> ${CHRONY_CONF_FILE}
     else
-      echo "server "${N_CLEANED}" iburst" >> ${CHRONY_CONF_FILE}
+      echo "server ${N_CLEANED} iburst" >> ${CHRONY_CONF_FILE}
     fi
   fi
 done
